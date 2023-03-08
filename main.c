d8aa1374ade112aedf7dbaed9376571d1add210a
Jared Vigliecca <jaredvig@gmail.com>
2023-03-08 09:46:10 +0000

    pain and suffering

--- main.c
+++ main.c
@@ -7,14 +7,115 @@
 
 int main(int argc, char *argv[]) {
 	double clockstart, clockend;
-	clockstart = rtclock(); // start clocking
+	if(argc<3||argc>4){
+		printf("Error, too many or too few arguments given, please give the proper amount of arguments.\n");
+		exit(1);
+	} 
+	if(strcmp(argv[1],"P") !=0 && strcmp(argv[1],"S")!=0){
+		printf("Error, please give a correct argument for either sequential (S) or parallel (P)\n");
+		exit(1);
+	}
+	//PARALLELIZED
+	if(strcmp(argv[1],"P")==0){
+		int length = atoi(argv[3]);
+		int threads = atoi(argv[2]);
+		if(threads>length){
+			printf("Error, the number of threads is larger than the size of the matrix.\n");
+			exit(1);
+		}
+		if(threads<0){
+		printf("Error, number of threads cannot be negative.\n");
+		exit(1);
+		}
+		if(length<0){
+		printf("Error, size of matrix cannot be negative.\n");
+		exit(1);
+		}
+		printf("==============\n");
+		printf("mode: parallel\n");
+		printf("thread count: %d\n",threads);
+		printf("size:%d\n", length);
+		printf("==============\n");
+		mmm_init(length);
+		pthread_t* tid = (pthread_t*)malloc(threads*sizeof(pthread_t));
+		for(int i = 0;i<threads;i++){
+			int* arguments = (int*)malloc(2*sizeof(int));
+			arguments[1] = threads;
+			arguments[0] = i;
+			//pthread_t thread;
+			//pthread_create(&thread,NULL,mmm_par,arguments);
+			pthread_create(&tid[i],NULL,mmm_par,arguments);
+		}
+		for(int i = 0;i<threads;i++){
+		pthread_join(tid[i],NULL);
+		}
+		free(tid);
 
+		clockstart = rtclock();
+		for(int i =0;i<3;i++){
+			pthread_t* tid = (pthread_t*)malloc(threads*sizeof(pthread_t));
+			for(int j = 0;j<threads;j++){
+				int* arguments = (int*)malloc(2*sizeof(int));
+				arguments[1] = threads;
+				arguments[0] = j;
+				pthread_t thread;
+				pthread_create(&thread,NULL,mmm_par,arguments);
+				tid[j] = thread;
+			}
+			for(int j = 0;j<threads;j++){
+			pthread_join(tid[j],NULL);
+			}
+			free(tid);
+		}
+		clockend = rtclock();
+		double totalTime= clockend-clockstart;
+		double avgTime = totalTime/3;
+		printf("Parallel Time (avg of 3 runs): %f sec\n",avgTime);
+		
+		mmm_seq();
+		clockstart = rtclock();
+		for(int i =0;i<3;i++){
+		mmm_seq();
+		}
+		clockend = rtclock();
+		totalTime= clockend-clockstart;
+		double avgTime2 = totalTime/3;
+		double speedup = avgTime2/avgTime;
+		printf("Sequential Time (avg of 3 runs): %f sec\n",avgTime2);
+		printf("Speedup: %f\n",speedup);
+		printf("Verifying...");
+		double largestError = mmm_verify();
+		printf("largest error between parallel and sequential matrix:%f\n",largestError);
+	} 
+	if(strcmp(argv[1],"S")==0){
+		int size = atoi(argv[2]);
+		if(size<0){
+		printf("Error, size of matrix cannot be negative.\n");
+		exit(1);
+		}
+		printf("==================\n");
+		printf("mode: sequential\n");
+		printf("thread count: 1\n");
+		printf("size:%d\n", size);
+		printf("==================\n");
+		mmm_init(size);
+		mmm_seq();
+		clockstart = rtclock();
+		for(int i =0;i<3;i++){
+		mmm_seq();
+		}
+		clockend = rtclock();
+		double totalTime= clockend-clockstart;
+		double avgTime = totalTime/3;
+		printf("Sequential Time (avg of 3 runs): %f sec\n",avgTime);
+	}
+	mmm_freeup();
 	// start: stuff I want to clock
 
 	// end: stuff I want to clock
 
-	clockend = rtclock(); // stop clocking
-	printf("Time taken: %.6f sec\n", (clockend - clockstart));
+	//clockend = rtclock(); // stop clocking
+	//printf("Time taken: %.6f sec\n", (clockend - clockstart));
 
 	return 0;
 }
--- mmm.c
+++ mmm.c
@@ -4,13 +4,33 @@
 #include <string.h>
 #include <stdio.h>
 #include "mmm.h"
+#include<pthread.h>
 
+double ** inputMatrix1;
+double ** inputMatrix2;
+double ** outputMatrixSeq;
+double ** outputMatrixPar;
+int globalSize;
 /**
  * Allocate and initialize the matrices on the heap. Populate
  * the input matrices with random integers from 0 to 99
  */
-void mmm_init() {
-	// TODO
+void mmm_init(int size) {
+	globalSize=size;
+	initMatrices();
+	mmmPopulateMatrices();
+}
+void initMatrices(){
+	inputMatrix1 = (double**)calloc(globalSize,sizeof(double*));
+	inputMatrix2 = (double**)calloc(globalSize,sizeof(double*));
+	outputMatrixSeq = (double**)calloc(globalSize,sizeof(double*));
+	outputMatrixPar = (double**)calloc(globalSize,sizeof(double*));
+	for(int i = 0; i<globalSize;i++){
+		inputMatrix1[i] = (double*)calloc(globalSize,sizeof(double));
+		inputMatrix2[i] = (double*)calloc(globalSize,sizeof(double));
+		outputMatrixSeq[i] = (double*)calloc(globalSize,sizeof(double));
+		outputMatrixPar[i] = (double*)calloc(globalSize,sizeof(double));
+	}
 }
 
 /**
@@ -19,13 +39,47 @@ void mmm_init() {
  */
 void mmm_reset(double **matrix) {
 	// TODO
+	for(int i = 0; i<globalSize; i++){
+		for(int j =0;j<globalSize; j++){
+			matrix[i][j] = 0;
+		}
+	}
+}
+void mmmPopulateMatrices(){
+	for(int i = 0; i<globalSize; i++){
+		for(int j = 0; j<globalSize; j++){
+		inputMatrix1[i][j] = (double)(rand()%100);
+		inputMatrix2[i][j] = (double)(rand()%100);
+	}
+	}
 }
 
 /**
  * Free up memory allocated to all matrices
  */
 void mmm_freeup() {
-	// TODO
+	for(int i = 0; i<globalSize;i++){
+		free(inputMatrix1[i]);
+		free(inputMatrix2[i]);
+		free(outputMatrixSeq[i]);
+		free(outputMatrixPar[i]);
+
+		inputMatrix1[i] = NULL;
+		inputMatrix1[i] = NULL;
+		inputMatrix1[i] = NULL;
+		inputMatrix1[i] = NULL;
+	}
+	free(inputMatrix1);
+	free(inputMatrix2);
+	free(outputMatrixSeq);
+	free(outputMatrixPar);
+	
+	inputMatrix1 = NULL;
+	inputMatrix1 = NULL;
+	outputMatrixSeq = NULL;
+	outputMatrixPar = NULL;
+
+
 }
 
 /**
@@ -33,13 +87,39 @@ void mmm_freeup() {
  */
 void mmm_seq() {
 	// TODO - code to perform sequential MMM
+	for(int i =0;i<globalSize;i++){
+		for(int j =0;j<globalSize;j++){
+			int outputCellValue = 0;
+			for(int k = 0; k<globalSize;k++){
+				outputCellValue+= inputMatrix1[i][k]*inputMatrix2[k][j];
+			}
+			outputMatrixSeq[i][j] = outputCellValue;
+		}
+	}
 }
 
 /**
  * Parallel MMM
  */
 void *mmm_par(void *args) {
+	int*input = (int*)args;
+	int startRow = input[0];
+	int threads = input[1];
+	for(int i = 0; i<globalSize;i++){
+		for(int j = startRow;j<globalSize;j+=threads){
+			int outputCellValue =0;
+			for(int k = 0;k<globalSize;k++){
+				outputCellValue+= inputMatrix1[i][k]*inputMatrix2[k][j];
+			}
+			outputMatrixPar[i][j] = outputCellValue;
+		}
+	}
+	 free(args);
+	 args=NULL;
+	return NULL;
+
 	// TODO - code to perform parallel MMM
+
 }
 
 /**
@@ -50,6 +130,18 @@ void *mmm_par(void *args) {
  * in the result matrices
  */
 double mmm_verify() {
-	// TODO
-	return -1;
-}
+	double largestDifference = 0;
+	for(int i = 0; i <globalSize; i++){
+		for(int j = 0;j <globalSize;j++){
+			//printf("%f",outputMatrixPar[i][j]);
+			//printf(" %f ",outputMatrixPar[i][j]);
+			double difference = abs(outputMatrixPar[i][j] - outputMatrixSeq[i][j]);
+			if(difference>largestDifference){
+				largestDifference = difference;
+			}
+			}
+		}
+		
+		return largestDifference;
+	}
+
